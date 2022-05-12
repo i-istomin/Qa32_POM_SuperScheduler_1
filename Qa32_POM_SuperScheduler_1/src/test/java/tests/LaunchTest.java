61b158dbeb7f1aabfa3f13193a98d6fcc1340271
i-istomin <missira85@gmail.com>
2022-05-12 11:16:25 +0300

    super scheduler

---
+++ Qa32_POM_SuperScheduler_1/src/test/java/tests/LaunchTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import configuration.AppiumConfiguration;
+import org.testng.annotations.Test;
+
+public class LaunchTest extends AppiumConfiguration {
+
+    @Test//budet ustoy, bez koda
+    public void launchTest(){
+
+    }
+}
